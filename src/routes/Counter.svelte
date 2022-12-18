51c102dca6180134458b53ceafac42d64b36ca4b
Emmanuel Mura <emmanuel.mura@gmail.com>
2022-12-18 14:05:25 +0700

    update sveltekit 1.0

--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -59,12 +59,11 @@
 		border: 0;
 		background-color: transparent;
 		touch-action: manipulation;
-		color: var(--text-color);
 		font-size: 2rem;
 	}
 
 	.counter button:hover {
-		background-color: var(--secondary-color);
+		background-color: var(--color-bg-1);
 	}
 
 	svg {
@@ -75,7 +74,7 @@
 	path {
 		vector-effect: non-scaling-stroke;
 		stroke-width: 2px;
-		stroke: var(--text-color);
+		stroke: #444;
 	}
 
 	.counter-viewport {
@@ -92,7 +91,7 @@
 		width: 100%;
 		height: 100%;
 		font-weight: 400;
-		color: var(--accent-color);
+		color: var(--color-theme-1);
 		font-size: 4rem;
 		align-items: center;
 		justify-content: center;
